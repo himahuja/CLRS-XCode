8b43a854cb98f19dd75795e21a8816f64cd5f57e
himanshuahuja8 <himanshuahuja@himanshus-macbook-pro.local>
2016-05-01 17:53:27 +0530

    First Commit

---
+++ HelloWorld/HelloWorld/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Himanshu Ahuja on 01/05/16.
+//  Copyright © 2016 Himanshu Ahuja. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("Hello World, the second time");
+    return 0;
+}
